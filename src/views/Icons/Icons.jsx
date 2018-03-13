2f2da00bb8a2efc992e5f8ade961ce666ae65ba6
weephat <jkweephat@gmail.com>
2018-03-13 20:04:09 +0700

    add comment card

--- src/views/Icons/Icons.jsx
+++ src/views/Icons/Icons.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
 
-import Card from 'components/Card/Card';
-import {iconsArray} from 'variables/Variables.jsx';
+
+
+
 
 class Icons extends Component {
     render() {
         return (
             <div className="content">
-               
+               Jsack
             </div>
         );
     }
